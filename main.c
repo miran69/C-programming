0fcd35606adc182cc2e0b3ccf65d32f3e89035e6
miran69 <khanmiran6969@eng.rizvi.edu.in>
2025-11-12 02:23:09 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/*Name: khan miran
+UIN: 251M002
+Class: F.E Mechanical 
+DIV: F*/
+#include <stdio.h>
+
+int main() {
+    char Name[]= "Khan miran";
+    int Age = 18; 
+    char Class[]= "F.E Mechanical";
+    char DIV[]= "F";
+    char UIN[]= "251M001"; 
+    
+    printf("Name\t: %s\n", Name);
+    printf("Age\t\t: %d\n", Age);
+    printf("Class\t: %s\n", Class);
+    printf("DIV\t\t: %s\n", DIV);
+    printf("UIN\t\t: %s\n", UIN);
+
+      return 0;
+}

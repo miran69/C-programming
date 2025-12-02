78dcbc4a775f377dfb8d170f266fc91fae3495d3
miran69 <khanmiran6969@eng.rizvi.edu.in>
2025-12-02 00:21:10 -0800

    Create Experiment 5.c

---
+++ Experiment 5.c
@@ -0,0 +1,38 @@
+/*Name: Khan Miran 
+UIN: 251M001
+Class: F.E Mechanical 
+DIV: F */
+#include <stdio.h>
+
+// Recursive function
+long long factorialRecursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorialRecursive(n - 1);
+}
+
+// Iterative function
+long long factorialIterative(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial (Recursive): %lld\n", factorialRecursive(num));
+        printf("Factorial (Iterative): %lld\n", factorialIterative(num));
+    }
+
+    return 0;
+}

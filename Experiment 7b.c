b80d1b75b2e2bda98f4a3e8f92106ef87e7db87b
miran69 <khanmiran6969@eng.rizvi.edu.in>
2025-12-02 00:27:43 -0800

    Create Experiment 7b.c

---
+++ Experiment 7b.c
@@ -0,0 +1,50 @@
+main.c
+
+// Name: Khan miran 
+// UIN: 25IM001
+// Class: F.E Mechanical
+// Div: F
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int a[rows][cols], b[rows][cols], sum[rows][cols];
+
+    printf("Enter elements of Matrix A:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of Matrix B:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nSum of Two Matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
